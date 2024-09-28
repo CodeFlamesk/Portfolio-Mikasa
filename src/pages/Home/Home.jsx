65ec290ba9b408a7fffda110894e3fee8a4ebbe2
CodeFlamesk <150174426+CodeFlamesk@users.noreply.github.com>
2024-09-28 15:58:39 +0200

    im

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,9 +5,9 @@
 const Home = () => {
     return (
         <>
-            <img src="/src/pages/Home/img/Fone.jpg" alt="1" className="w-screen h-screen object-cover" />
-            <img src="/src/pages/Home/img/Fone2.png" alt="2" className="w-screen h-screen object-cover" />
-            <img src="/src/pages/Home/img/Fone3.png" alt="3" className="w-screen h-screen object-cover" />
+            <img src="/src/components/img/Fone.jpg" alt="1" className="w-screen h-screen object-cover" />
+            <img src="/src/components/img/Fone2.png" alt="2" className="w-screen h-screen object-cover" />
+            <img src="/src/components/img/Fone3.png" alt="3" className="w-screen h-screen object-cover" />
 
 
 
