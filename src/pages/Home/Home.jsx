b088163650792bed7bad44b57e89b8f488708f89
CodeFlamesk <150174426+CodeFlamesk@users.noreply.github.com>
2024-09-29 12:52:28 +0200

    mikasa img

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,6 +8,7 @@ const Home = () => {
             <img src="/img/Fone.jpg" alt="1" className="w-screen h-screen object-cover" />
             <img src="/img/Fone2.png" alt="2" className="w-screen h-screen object-cover" />
             <img src="/img/Fone3.png" alt="3" className="w-screen h-screen object-cover" />
+            <img src="/img/Fone4.png" alt="3" className="w-screen h-screen object-cover" />
 
 
 
