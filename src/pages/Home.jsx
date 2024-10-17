a9559d87794cffe6c1f3d4cbd7852bc852c68840
CodeFlamesk <150174426+CodeFlamesk@users.noreply.github.com>
2024-10-17 14:28:49 +0200

    img w h

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,22 +12,26 @@ const Home = () => {
             <div className='flex flex-col items-center gap-6 pt-16 lg:pt-0'>
                 <Link to="/home/about" className=" btn flex items-end   lg:mt-16 ">
                     Mikasa
-                    <img src="/homeImg/aboutLogo.webp" alt="aboutLogo" className=" w-[35px] h-[44px] lg:w-full lg:h-full " />
+                    <img src="/homeImg/aboutLogo.webp" alt="aboutLogo" className=" w-[35px] h-[44px] lg:w-full lg:h-full " width="35"
+                        height="44" />
 
                 </Link>
                 <div className="flex flex-col lg:flex-row items-center gap-y-6 gap-x-20  ">
                     <Link to="/home/skills" className=" btn flex items-baseline  lg:mt-16 py-5 ">
                         Skills
-                        <img src="/homeImg/skillsLogo.webp" alt="aboutLogo" className='rotate   w-[40px] h-[40px] lg:w-full lg:h-full' />
+                        <img src="/homeImg/skillsLogo.webp" alt="aboutLogo" className='rotate   w-[40px] h-[40px] lg:w-full lg:h-full' width="40"
+                            height="40" />
                     </Link>
                     <Link to="/home/contact" className=" btn flex items-baseline   lg:mt-16 ">
                         Contact
-                        <img src="/homeImg/message.webp" alt="aboutLogo" className=" w-[40px] h-[32px] lg:w-[78px] lg:h-[63px] " />
+                        <img src="/homeImg/message.webp" alt="aboutLogo" className=" w-[40px] h-[32px] lg:w-[78px] lg:h-[63px] " width="40"
+                            height="32" />
                     </Link>
                 </div>
                 <Link to="/home/projects" className=" btn flex items-baseline md:mb-10 lg:mb-16 lg:mt-16 ">
                     Projects
-                    <img src="/homeImg/projects.webp" alt="aboutLogo" className='scale  w-[45px] h-[29px] lg:w-[91px] lg:h-[59px] ' />
+                    <img src="/homeImg/projects.webp" alt="aboutLogo" className='scale  w-[45px] h-[29px] lg:w-[91px] lg:h-[59px] ' width="45"
+                        height="29" />
                 </Link>
 
 
@@ -35,7 +39,8 @@ const Home = () => {
                     <div className='flex flex-row justify-center md:flex-col xl:flex-row gap-1 md:gap-3 items-center  bg-opacity-90 text-center rounded-3xl  marquee md:animate-none '>
                         <p className='main-text whitespace-nowrap'>Give your</p>
                         <div className='min-w-[80px] min-h-[60px] md:min-h-[80px] md:w-24 flex items-center justify-center'>
-                            <img src="/homeImg/heart.webp" alt="aboutLogo" className='heart min-w-[52px] h-[52px] xl:w-[72px] xl:h-[72px]' />
+                            <img src="/homeImg/heart.webp" alt="aboutLogo" className='heart min-w-[52px] h-[52px] xl:w-[72px] xl:h-[72px]' width="52"
+                                height="52" />
                         </div>
                         <p className='main-text whitespace-nowrap'>to a front-end developer</p>
                     </div>
