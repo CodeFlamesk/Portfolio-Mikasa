bb386a41eed698ede2bb0347fac80bd58cb9eee5
CodeFlamesk <150174426+CodeFlamesk@users.noreply.github.com>
2024-11-20 15:14:42 +0100

    fix seo2

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,25 +1,23 @@
-import React, { lazy, Suspense } from 'react';
+import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from '@pages/Home';
-
-const Skills = lazy(() => import('@pages/Skills'));
-const About = lazy(() => import('@pages/About'));
-const Projects = lazy(() => import('@pages/project/Projects'));
-const ProjectCard = lazy(() => import('@pages/project/ProjectCard'));
-const Contact = lazy(() => import('@pages/Contact'));
+import Skills from '@pages/Skills';
+import About from '@pages/About';
+import Projects from '@pages/project/Projects';
+import ProjectCard from '@pages/project/ProjectCard';
+import Contact from '@pages/Contact';
 
 const AppRouter = () => {
     return (
-      
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/home/about" element={<About />} />
-                <Route path="/home/skills" element={<Skills />} />
-                <Route path="/home/projects" element={<Projects />} />
-                <Route path="/home/projectscard" element={<ProjectCard />} />
-                <Route path="/home/contact" element={<Contact />} />
-            </Routes>
-     
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/home/about" element={<About />} />
+            <Route path="/home/skills" element={<Skills />} />
+            <Route path="/home/projects" element={<Projects />} />
+            <Route path="/home/projectscard" element={<ProjectCard />} />
+            <Route path="/home/contact" element={<Contact />} />
+        </Routes>
     );
 };
+
 export default AppRouter;
